776e076ddbda8e0d1608e01a15aa08a8a120a5c6
Philip <philipmjobe@gmail.com>
2022-02-01 15:10:32 -0500

    init for money.rb

---
+++ money.rb
@@ -0,0 +1,7 @@
+# Given a string, write a function that returns true if the “$” character is contained within the string or false if it is not.
+
+# Input: “i hate $ but i love money i know i know im crazy”
+# Output: true
+
+# Input: “abcdefghijklmnopqrstuvwxyz”
+# Output: false
