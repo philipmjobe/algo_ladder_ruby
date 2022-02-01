5ef27a80cfc662c3f48766a3537dccf0584da9b4
Philip <philipmjobe@gmail.com>
2022-02-01 15:00:17 -0500

    added answer for reverse_string.rb

--- reverse_string.rb
+++ reverse_string.rb
@@ -2,3 +2,15 @@
 
 # Input: “abcde”
 # Output: “edcba”
+
+def reverse(string)
+  i = 0
+  result = ""
+  while i < string.length
+    result = string[i] + result
+    i += 1
+  end 
+  return result
+end 
+
+p reverse("abcde")
