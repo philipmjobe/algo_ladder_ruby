22c0d3dd4db898c9ee5699d372cfc0f011a9ef45
Philip <philipmjobe@gmail.com>
2022-02-02 16:15:03 -0500

    init for reverse_words.rb

---
+++ reverse_words.rb
@@ -0,0 +1,4 @@
+# Given a string of words, write a function that returns a new string that contains the words in reverse order.
+
+# Input: “popcorn is so cool isn’t it yeah i thought so”
+# Output: “so thought i yeah it isn’t cool so is popcorn”
