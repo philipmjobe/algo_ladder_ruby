3ce0850e8002f22b1ff133e14d934672eac977f6
Philip <philipmjobe@gmail.com>
2022-02-02 16:19:13 -0500

    adds answer for reverse_words.rb

--- reverse_words.rb
+++ reverse_words.rb
@@ -2,3 +2,14 @@
 
 # Input: “popcorn is so cool isn’t it yeah i thought so”
 # Output: “so thought i yeah it isn’t cool so is popcorn”
+
+def reverse_string(string)
+  reversed_string = ""
+  i = string.length - 1
+  while i >= 0
+    reversed_string += string[i]
+    i -= 1
+  end 
+  return reversed_string
+end
+p reverse_string("popcorn is so cool isn't it yeah i thought so")
