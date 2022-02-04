c9e6dd9d98d893cf6e2d36e7174ce9ed6aab3172
Philip <philipmjobe@gmail.com>
2022-02-04 15:17:07 -0500

    adds answer for fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,3 +3,18 @@
 # If the number is divisible by 3, print out "FIZZ".
 # If the number is divisible by 5, print out "BUZZ".
 # If the number is divisible by both 3 and 5, print out "FIZZBUZZ".
+
+def fizz?(input)
+  if input % 15 == 0
+    output = "FIZZBUZZ"
+  elsif input % 5 == 0
+    output = "FIZZ"
+  elsif input % 3 == 0
+    output = "BUZZ"
+  else
+    output = "NOPE"
+  end
+  return output
+end 
+
+puts fizz?(150)
