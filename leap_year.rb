6b8b07f459f67882121d62dc12d527cfb9ac3054
Philip <philipmjobe@gmail.com>
2022-04-08 14:51:29 -0400

    adds answer for leap_year.rb

--- leap_year.rb
+++ leap_year.rb
@@ -8,3 +8,11 @@
 # For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
 
 # If your language provides a method in the standard library that does this look-up, pretend it doesn't exist and implement it yourself.
+
+def leap(i)
+  return true if i % 400 == 0
+  return false if i % 100 == 0
+  return i % 4 == 0
+end
+
+p leap(1996)
