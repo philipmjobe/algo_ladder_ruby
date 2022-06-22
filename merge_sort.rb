3f7efd07708fcac09cd93ee9c4cf2f85a9c39aec
Philip <philipmjobe@gmail.com>
2022-06-22 16:00:15 -0400

    adds merge_sort.rb and question

---
+++ merge_sort.rb
@@ -0,0 +1,7 @@
+# Given two sorted arrays, merge the second array into the first array while ensuring that the first array remains sorted. Do not use any built-in sort methods.
+
+# Input :
+# A : [1, 5, 8]
+# B : [6, 9]
+
+# Modified A : [1, 5, 6, 8, 9]
