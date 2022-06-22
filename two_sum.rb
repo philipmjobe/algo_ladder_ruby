adc034da86b9f83abe883eaf5ba0ad9ec1b583a2
Philip <philipmjobe@gmail.com>
2022-06-22 15:28:42 -0400

    adds answer for two_sum.rb

--- two_sum.rb
+++ two_sum.rb
@@ -7,3 +7,25 @@
 
 # Input: [1, 2, 3, 4, 5]
 # Output: false (While 1, 2, 3, and 4 altogether add up to 10, we're seeking just one pair of numbers.)
+
+def sums(nums)
+  i = 0 
+  sum_arr = []
+  while i < nums.length
+    j = 0
+    while j < nums.length
+      if i != j
+        if nums[i] + nums[j] == 10
+          sum_arr.push(nums[i], nums[j])
+          return sum_arr
+        end
+      end
+      j += 1
+    end
+    i += 1
+  end
+  return false
+end
+
+p sums([2, 5, 3, 1, 0, 7, 11])
+p sums([1, 2, 3, 4, 5])
