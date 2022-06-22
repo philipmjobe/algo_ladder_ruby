87cc14ebaf02e5614fdec865ada97fb3fa29ca38
Philip <philipmjobe@gmail.com>
2022-06-22 15:20:10 -0400

    adds answer for largest_product.rb

--- largest_product.rb
+++ largest_product.rb
@@ -2,3 +2,23 @@
 
 # Input: [5, -2, 1, -9, -7, 2, 6]
 # Output: 63 (-9 * -7)
+
+def largest(num_arr)
+  so_far = 0
+  i = 0
+  while i < num_arr.length
+    j = 0
+    while j < num_arr.length
+      if i != j
+        if num_arr[i] * num_arr[j] > so_far
+          so_far = num_arr[i] * num_arr[j]
+        end
+      end 
+        j += 1
+    end
+      i += 1
+  end 
+  so_far
+end
+
+p largest([5, -2, 1, -9, -7, 2, 6])
