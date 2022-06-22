b6ed7eca803d2b5c42a7637caea9d58eec5d7dc1
Philip <philipmjobe@gmail.com>
2022-06-22 15:08:04 -0400

    adds answer for arr_mesh.rb

--- arr_mesh.rb
+++ arr_mesh.rb
@@ -2,3 +2,21 @@
 
 # Input: ["a", "b", "c", "d"]
 # Output: ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"]
+
+def mesh(arr)
+  pairs = []
+  i = 0
+  while i < arr.length
+    j = 0 
+    while j < arr.length
+      if i != j
+        pairs << arr[i] + arr[j]
+      end
+      j += 1
+    end 
+    i += 1
+  end 
+  pairs
+end 
+
+p mesh(["a", "b", "c", "d"])
