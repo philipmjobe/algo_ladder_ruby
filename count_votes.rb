2883ccaafa7e241f12ac3278d3e93909ab9556d0
Philip <philipmjobe@gmail.com>
2022-06-27 16:11:08 -0400

    adds answer for count_votes.rb

--- count_votes.rb
+++ count_votes.rb
@@ -5,3 +5,19 @@
 # Output: {"Dewey" => 6, "Truman" => 5}
 
 # Explanation: "Dewey" occurs 6 times in the array, while "Truman" occurs 5 times.
+
+def count(arr)
+  votes_hash = {}
+  
+  arr.each do |vote|
+    lowercase_vote = vote.downcase
+    if votes_hash[lowercase_vote]
+      votes_hash[lowercase_vote] += 1
+    else  
+      votes_hash[lowercase_vote] = 1
+    end 
+  end
+  votes_hash
+end 
+
+p count(["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"])
