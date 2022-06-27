d6d7998d4a12f36b93dfa09534287bca9f6748f8
Philip <philipmjobe@gmail.com>
2022-06-27 16:53:10 -0400

    adds answer for anagrams.rb

--- anagrams.rb
+++ anagrams.rb
@@ -7,3 +7,32 @@
 
 # Input: “frog”, “bear”
 # Output: false
+
+def anagrams(str1, str2)
+  letter_count = {}
+  str1.each_char do |char|
+    if letter_count[char]
+      letter_count[char] += 1
+    else  
+      letter_count[char] = 1
+    end
+  end
+
+  str2.each_char do |char2|
+    if letter_count[char2]
+      letter_count[char2] -= 1
+    else  
+      return false
+    end
+  end
+  letter_array = letter_count.values
+  letter_array.each do |count|
+    if count != 0 
+      return false
+    end
+  end 
+  return true
+end
+
+p anagrams(“silent”, “listen”)
+p anagrams(“frog”, “bear”)
