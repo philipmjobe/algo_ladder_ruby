4e0741e5134d1716e60908d10b7125d6d56502b5
Philip <philipmjobe@gmail.com>
2022-06-27 16:07:39 -0400

    adds count_votes.rb and question

---
+++ count_votes.rb
@@ -0,0 +1,7 @@
+# Given an array of strings, return a hash that provides of a count of how many times each string occurs.
+
+# Input: ["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"]
+
+# Output: {"Dewey" => 6, "Truman" => 5}
+
+# Explanation: "Dewey" occurs 6 times in the array, while "Truman" occurs 5 times.
