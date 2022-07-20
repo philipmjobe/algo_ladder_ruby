5b0ca8a6dee20c9ab4596fbbc00a07bb36943026
Philip <philipmjobe@gmail.com>
2022-07-20 14:56:55 -0400

    adds answer for flatten_hash.rb

--- flatten_hash.rb
+++ flatten_hash.rb
@@ -2,3 +2,10 @@
 
 # Input: {“a” => 1, “b” => 2, “c” => 3, “d” => 4}
 # Output: [“a”, 1, “b”, 2, “c”, 3, “d”, 4]
+
+def flatten(hash)
+  arr_hash = hash.to_a
+  arr_hash.flatten
+end
+
+p flatten({"a" => 1, "b" => 2, "c" => 3, "d" => 4})
