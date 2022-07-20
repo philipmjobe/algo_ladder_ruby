43b3878e0679195d8f94d442aa232610967c7cfb
Philip <philipmjobe@gmail.com>
2022-07-20 14:58:28 -0400

    adds flip_hash.rb

---
+++ flip_hash.rb
@@ -0,0 +1,11 @@
+# # Given a hash, return a flat array containing all the hash’s keys and values.
+
+# # Input: {“a” => 1, “b” => 2, “c” => 3, “d” => 4}
+# # Output: [“a”, 1, “b”, 2, “c”, 3, “d”, 4]
+
+# def flatten(hash)
+#   arr_hash = hash.to_a
+#   arr_hash.flatten
+# end
+
+# p flatten({"a" => 1, "b" => 2, "c" => 3, "d" => 4})
