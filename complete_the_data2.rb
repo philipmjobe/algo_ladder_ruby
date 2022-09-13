dc5ddbbec0c4af7e14aa2a11a6ff0846f7acfe8e
Philip <philipmjobe@gmail.com>
2022-09-13 14:41:53 -0400

    adds answer for complete the data 2

--- complete_the_data2.rb
+++ complete_the_data2.rb
@@ -26,3 +26,30 @@
 # {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
 # {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
 # ]
+
+def data2(posts, users)
+  users_hash = Hash.new()
+  users.each do |user|
+    users_hash[user[:user_id]] = user[:name]
+  end
+
+  posts.map do |post|
+    post[:submitted_by] = users_hash[post[:submitted_by]]
+  end
+  posts
+end 
+
+p data2(
+  [
+    {title: 'Me Eating Pizza', submitted_by: 231, likes: 1549},
+    {title: 'i never knew how cool i was until now', submitted_by: 989, likes: 3},
+    {title: 'best selfie evar!!!', submitted_by: 111, likes: 1092},
+    {title: 'Mondays are the worst', submitted_by: 403, likes: 644}
+  ],
+  [
+    {user_id: 403, name: "Aunty Em"},
+    {user_id: 231, name: "Joelle P."},
+    {user_id: 989, name: "Lyndon Johnson"},
+    {user_id: 111, name: "Patti Q."},
+  ]
+)
